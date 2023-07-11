50c8d2bb00c0dde0349d111c9f01aa1664c72bea
GabiHert <hertergabriel4@gmail.com>
2023-07-11 19:04:04 -0300

    removed unused functions and consts [refactor]

--- src/controller/request-controller.cpp
+++ src/controller/request-controller.cpp
@@ -3,7 +3,6 @@
 #include "config/config.h"
 #include "domain/cb/cb.h"
 #include "controller/request-controller.h"
-#include "useCase/clear-wheel-bolts-counter-use-case.h"
 #include "types/error-or-string.h"
 
 RequestController::RequestController(Cb *cb, IGps *gps, ILcd *lcd)
@@ -15,10 +14,6 @@ RequestController::RequestController(Cb *cb, IGps *gps, ILcd *lcd)
     loggerInfo("RequestController", "CONSTRUCTOR", "cbId: " + this->cb->getId());
 
     this->doseUseCase = DoseUseCase(cb, this->lcd);
-
-    this->turnAlarmSirenOnUseCase = TurnAlarmSirenOnUseCase(cb, this->lcd);
-
-    this->clearWheelBoltsCounterUseCase = ClearWheelBoltsCounterUseCase(cb);
 
     this->getGpsLocationUseCase = GetGpsLocationUseCase(gps, cb, this->lcd);
 };
@@ -33,36 +28,12 @@ ErrorOrResponseDto RequestController::execute(RequestDto requestDto)
     RequestModel requestModel(requestDto);
     this->cb->setRequestModel(requestModel);
 
-    bool turnAlarmSirenOnRequest = requestModel.getAlarmSiren() != CONFIG_PROTOCOL_ALARM_SIREN_OFF;
     bool doseRequest = requestModel.getDose() != CONFIG_PROTOCOL_DO_NOT_DOSE;
-    bool clearWheelBoltsCounterRequest = requestModel.getWheelBoltsCounter() != CONFIG_PROTOCOL_DO_NOT_CLEAR_WHELL_BOLT_COUNTS;
-
-    if (turnAlarmSirenOnRequest)
-    {
-        loggerInfo("RequestController.execute", "Turn alarm siren on Request detected");
-        ErrorOrBool errorOrBool = this->turnAlarmSirenOnUseCase.execute();
-        if (errorOrBool.isError())
-        {
-            loggerError("requestController.execute", "Process error", "error: " + errorOrBool.getError().description);
-            return ErrorOrResponseDto(errorOrBool.getError());
-        }
-    };
 
     if (doseRequest)
     {
         loggerInfo("RequestController.execute", "Dose request detected");
         ErrorOrBool errorOrBool = this->doseUseCase.execute(requestModel.getDose());
-        if (errorOrBool.isError())
-        {
-            loggerError("requestController.execute", "Process error", "error: " + errorOrBool.getError().description);
-            return ErrorOrResponseDto(errorOrBool.getError());
-        }
-    };
-
-    if (clearWheelBoltsCounterRequest)
-    {
-        loggerInfo("RequestController.execute", "Clear wheel bolts counter request detected");
-        ErrorOrBool errorOrBool = this->clearWheelBoltsCounterUseCase.execute();
         if (errorOrBool.isError())
         {
             loggerError("requestController.execute", "Process error", "error: " + errorOrBool.getError().description);
--- src/controller/request-controller.h
+++ src/controller/request-controller.h
@@ -2,9 +2,7 @@
 #define REQUEST_CONTROLLER_H
 #include "domain/cb/cb.h"
 #include <Arduino.h>
-#include "useCase/turn-alarm-siren-on-use-case.h"
 #include "useCase/dose-use-case.h"
-#include "useCase/clear-wheel-bolts-counter-use-case.h"
 #include "useCase/get-gps-location-use-case.h"
 #include "domain/dto/response-dto.h"
 #include "domain/dto/request-dto.h"
@@ -15,9 +13,7 @@ class RequestController
 private:
     Cb *cb;
     ILcd *lcd;
-    TurnAlarmSirenOnUseCase turnAlarmSirenOnUseCase;
     DoseUseCase doseUseCase;
-    ClearWheelBoltsCounterUseCase clearWheelBoltsCounterUseCase;
     GetGpsLocationUseCase getGpsLocationUseCase;
 
 public:
